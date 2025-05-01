562fa2f6c98482263ce6295053f717e0226cc815
ambuj vashistha <ambujva123@gmail.com>
2025-05-02 00:28:08 +0530

    Initial commit for MoodMorph

---
+++ moods.js
@@ -0,0 +1,26 @@
+export const moods = {
+  polite: {
+    responses: [
+      "Thank you for sharing. Please continue.",
+      "That's quite thoughtful.",
+      "Would you like to elaborate?",
+    ],
+    className: "polite",
+  },
+  sassy: {
+    responses: [
+      "Ugh, is that all?",
+      "Try harder, darling 💅",
+      "I’ve heard better from a toaster.",
+    ],
+    className: "sassy",
+  },
+  cheerful: {
+    responses: [
+      "Yay! That’s awesome!",
+      "Keep that energy up! ✨",
+      "You're doing great!",
+    ],
+    className: "cheerful",
+  }
+};
